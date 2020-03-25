34a2fb5f0ae5bd7a4c66822ed8e8d4c4a3ca7683
Indika Piyasena <indika.piyasena@gmail.com>
2020-03-26 07:45:41 +0800

    generate tables for changes in last 24H

--- src/Report.hs
+++ src/Report.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Report
-  ( totalSupplyDX
+  ( tableTotalSupply24H
+  , tableAccounts24H
   ) where
 
-import           Config                      (accounts, connectionString)
-import           Data.Time.Clock             (UTCTime, addUTCTime,
-                                              getCurrentTime)
+import           Config                          (accounts, connectionString)
+import           Data.Time.Clock                 (UTCTime, addUTCTime,
+                                                  getCurrentTime)
 import           Database.PostgreSQL.Simple
 
-import           Text.Blaze.Html5            as H
-import           Text.Blaze.Html5.Attributes as A
+import           Control.Monad                   (forM_)
+import           Text.Blaze.Html.Renderer.String (renderHtml)
+import           Text.Blaze.Html5                as H
+import           Text.Blaze.Html5.Attributes     as A
 
 obtainSample :: Connection -> String -> String -> (UTCTime, UTCTime) -> IO Int
 obtainSample c metric feature (a, b) = do
@@ -28,13 +31,13 @@ obtainSample c metric feature (a, b) = do
 window :: IO (UTCTime, UTCTime)
 window = do
   now <- getCurrentTime
-  return $ (addUTCTime (-3600 * 24) now, now)
+  return $ (addUTCTime (-3600 * 24 * 1) now, now)
 
-totalSupplyDX = do
+metricDX metric feature = do
   now <- window
   cs <- connectionString
   conn <- connectPostgreSQL cs
-  obtainSample conn "supply" "total" now
+  obtainSample conn metric feature now
 
 accountDX = do
   now <- window
@@ -42,7 +45,31 @@ accountDX = do
   conn <- connectPostgreSQL cs
   mapM (\x -> obtainSample conn "account" x now) accounts
 
-test = do
+tableAccounts24H = do
+  accResults <- accountDX
+  let tableHead = thead (th "Account Number" >> th "Amount in nUND")
+  let lns = zip (toHtml <$> accounts) (toHtml <$> accResults)
+  return $
+    renderHtml (table ! class_ "statstable" $ (tableHead >> (mapM_ c lns)))
+  where
+    c ln = tr (td (fst ln) >> td (snd ln))
+
+tableTotalSupply24H = do
+  supplyAmountChange <- metricDX "supply" "amount"
+  supplyTotalChange <- metricDX "supply" "total"
+  supplyLockedChange <- metricDX "supply" "locked"
+  let tableHead = thead (th "Total" >> th "Amount in nUND")
+  let lns =
+        [ ("Supply Amount Change", toHtml supplyAmountChange)
+        , ("Supply Total Change", toHtml supplyTotalChange)
+        , ("Supply Locked Change", toHtml supplyLockedChange)
+        ]
+  return $
+    renderHtml (table ! class_ "statstable" $ (tableHead >> (mapM_ c lns)))
+  where
+    c ln = tr (td (fst ln) >> td (snd ln))
+
+test' = do
   now <- window
   print $ show now
   cs <- connectionString
