f580072b8b25648b2d2be2f7566a2dc6a76ead3f
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-06 19:25:33 +1000

    read secrets from a .secrets file

---
+++ src/Secrets.hs
@@ -0,0 +1,22 @@
+module Secrets
+  (
+  ) where
+
+import           Data.List        (elemIndex)
+import           System.Directory (getHomeDirectory)
+import           System.FilePath  ((</>))
+
+filename = ".secrets"
+
+getSecret :: String -> IO (Maybe String)
+getSecret key = do
+  base <- getHomeDirectory
+  x <- readFile (base </> filename)
+  let filtered = filter (\x -> length x > 0) (lines x)
+  case length filtered of
+    0 -> return $ Nothing
+    _ -> do
+      let xs = words <$> filtered
+      case elemIndex key (head <$> xs) of
+        Nothing  -> return $ Nothing
+        Just (x) -> return $ Just ((xs !! x) !! 1)
