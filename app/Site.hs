cecf778a4d1d58ffd4878ead85b2581221e40426
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-08 16:20:46 +1000

    lint

--- app/Site.hs
+++ app/Site.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+module Main where
+
 import           Data.Monoid (mappend)
 import           Hakyll
-import           Report      (tableTotalSupply24H, tableAccounts24H, tableValidators24H)
+import           Report      (tableAccounts24H, tableTotalSupply24H,
+                              tableValidators24H)
 
 main :: IO ()
 main = do
@@ -11,8 +14,8 @@ main = do
   dataTotalSupply24H <- tableTotalSupply24H
   hakyll $ do
     match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+      route idRoute
+      compile copyFileCompiler
     match "css/*" $ do
       route idRoute
       compile compressCssCompiler
@@ -20,9 +23,10 @@ main = do
       route idRoute
       compile $ do
         let indexCtx =
-              constField "title" "Stats" `mappend` constField "dataAccounts24H" dataAccounts24H `mappend` 
-              constField "dataValidators24H" dataValidators24H `mappend` 
-              constField "dataTotalSupply24H" dataTotalSupply24H `mappend` 
+              constField "title" "Stats" `mappend`
+              constField "dataAccounts24H" dataAccounts24H `mappend`
+              constField "dataValidators24H" dataValidators24H `mappend`
+              constField "dataTotalSupply24H" dataTotalSupply24H `mappend`
               defaultContext
         getResourceBody >>= applyAsTemplate indexCtx >>=
           loadAndApplyTemplate "templates/default.html" indexCtx >>=
