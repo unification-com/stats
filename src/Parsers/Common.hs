1774a2adf3bdd54be7ab2cb43dd5a5194eb638fd
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-12 12:37:39 +1000

    subselect salt via JQ

--- src/Parsers/Common.hs
+++ src/Parsers/Common.hs
@@ -1,8 +1,10 @@
 module Parsers.Common
   ( restEndpoint
   , curljq
+  , saltjq
   ) where
 
+import           Data.List      (intercalate)
 import           System.IO      (hGetContents)
 import           System.Process
 
@@ -18,3 +20,26 @@ curljq url query = do
   return $ lines output
   where
     shell_cmd = "curl -s " ++ url ++ " | jq -r '" ++ query ++ "'"
+
+saltjq :: String -> [String] -> String -> IO [String]
+saltjq grain args query = do
+  print $ shell_cmd
+  (_, Just hOut, _, hProc) <-
+    createProcess ((shell (shell_cmd)) {std_out = CreatePipe})
+  exitCode <- waitForProcess hProc
+  output <- hGetContents hOut
+  return $ lines output
+  where
+    shell_cmd = saltCmd grain args query
+
+saltCmd grain args query =
+  saltSection grain ++ " | jq " ++ argStr ++ " '" ++ query ++ "' "
+  where
+    saltSection grain =
+      "/usr/local/bin/salt-call --local " ++
+      grain ++ " --output json --log-level error"
+    argStr = intercalate " " $ jqArgs args
+    jqArgs args =
+      (\(x, y) -> "--arg " ++ [y] ++ " '" ++ x ++ "'") <$> (zip args vars)
+    vars = take (length args) ['a' ..]
+
