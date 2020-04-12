a498d0d1d08de7bf6c20e3712e5840da1dfdcf82
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-12 12:38:22 +1000

    wire in reading disk usage

--- src/Trawl.hs
+++ src/Trawl.hs
@@ -9,6 +9,8 @@ import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 import           Network.HTTP.Types.Status (statusCode)
 
+import           Parsers.Common            (saltjq)
+
 ingestionEnpoint = "https://ingest-testnet.unification.io/ingest"
 
 submit :: String -> String -> String -> String -> String -> String -> IO ()
@@ -38,7 +40,14 @@ submit secret machine datatype metric key sample = do
 
 ping secret machine = submit secret machine "string" "ping" "ping" "ping"
 
+diskUsage secret machine = do
+  out <- saltjq "disk.usage" ["/", "1K-blocks"] ".local[$a] | .used,.[$b]"
+  print $ show out
+  submit secret machine "integer" "DiskUsage" "Used" (out !! 0)
+  submit secret machine "integer" "DiskUsage" "1KBlocks" (out !! 1)
+
 trawl :: String -> String -> IO ()
 trawl secret machine = do
   ping secret machine
+  diskUsage secret machine
   print "Done trawling"
