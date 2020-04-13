c05fe372cb8ef1c391daf5503efc46d79b4b6317
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-13 23:59:04 +1000

    neatening query extraction

--- src/Queries.hs
+++ src/Queries.hs
@@ -1,25 +1,40 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Queries
   ( lastestZQuery
+  , FeatureQuery
+  , Window
   ) where
 
 import qualified Data.Text                        as T
 import           Data.Text.Encoding               (encodeUtf8)
+import           Data.Time.Clock                  (UTCTime)
+import           Database.PostgreSQL.Simple       (Connection, query)
 import           Database.PostgreSQL.Simple.Types (Query (..))
+import           Text.Printf                      (printf)
 import           Text.RawString.QQ
 
-lastestZQuery :: Query
-lastestZQuery =
-  Query
-    ((encodeUtf8 . T.pack)
-       [r|
+type Window = (UTCTime, UTCTime)
 
-SELECT DISTINCT ON (N1.machine) machine, sample FROM stats.metrics N1
-INNER JOIN (
-    SELECT DISTINCT(machine) as machinej FROM stats.metrics
-    WHERE metric = ? AND feature = ? AND utc_date > ? AND utc_date < ?) N2
-    ON (N1.machine = N2.machinej)
-ORDER BY N1.machine, utc_date DESC;
-|])
+type FeatureQuery = (String, String, String, Window)
+
+queryFromString x = Query ((encodeUtf8 . T.pack) x)
+
+lastestZQuery :: Connection -> FeatureQuery -> IO [(String, Int)]
+lastestZQuery c (table, metric, feature, (a, b)) =
+  query
+    c
+    (queryFromString $
+     (printf
+        [r|
+
+  SELECT DISTINCT ON (N1.machine) machine, sample FROM %s N1
+  INNER JOIN (
+      SELECT DISTINCT(machine) as machinej FROM %s
+      WHERE metric = ? AND feature = ? AND utc_date > ? AND utc_date < ?) N2
+      ON (N1.machine = N2.machinej)
+  ORDER BY N1.machine, utc_date DESC;
+  |]
+        table
+        table)) $
+  [metric, feature, show a, show b]
