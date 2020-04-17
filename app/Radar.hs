c9e10391fb0c8c522a0c41b3f9454642c9db3a35
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-18 09:32:13 +1000

    refactor common code

--- app/Radar.hs
+++ app/Radar.hs
@@ -6,32 +6,24 @@ import           Radar.Scan         (scan, scanPorts)
 import           Radar.Slack        (postToSlack)
 import           Secrets            (getSecret)
 
-fn :: [Char] -> [String] -> IO ()
-fn "internal" args = do
+evaluate scanner = do
   slackChannel <- getSecret "TestAPI"
   case slackChannel of
     Nothing -> print "Slack secret not found"
     Just channel -> do
-      ret <- scanPorts
+      ret <- scanner
       case ret of
         Left xs  -> postToSlack channel xs
         Right xs -> print xs
+
+fn :: [Char] -> [String] -> IO ()
+fn "internal" args = evaluate scanPorts
 fn _ _ = do
   print "internal"
   return $ ()
 
-compute = do
-  slackChannel <- getSecret "TestAPI"
-  case slackChannel of
-    Nothing -> print "Slack secret not found"
-    Just channel -> do
-      ret <- scan
-      case ret of
-        Left xs  -> postToSlack channel xs
-        Right xs -> print xs
-
 main = do
   args <- getArgs
   if length args == 0
-    then compute
+    then evaluate scan
     else fn (head args) (tail args)
