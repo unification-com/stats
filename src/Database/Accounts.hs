461052ddbef3a4e619e431af6f2ad9cfdacfd075
Indika Piyasena <indika.piyasena@gmail.com>
2020-05-27 13:20:00 +1000

    interface to the accounts table

---
+++ src/Database/Accounts.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Database.Accounts
+  ( allAccounts
+  ) where
+
+import           Config                             (connectionString)
+
+import           Data.Int                           (Int64)
+import           Data.Text                          (Text, pack)
+
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.FromRow
+import           Database.PostgreSQL.Simple.ToField
+import           Database.PostgreSQL.Simple.ToRow
+
+data Account =
+  Account
+    { address :: String
+    , name    :: String
+    }
+  deriving (Show)
+
+instance FromRow Account where
+  fromRow = Account <$> field <*> field
+
+instance ToRow Account where
+  toRow t = [toField (address t), toField (name t)]
+
+allAccounts :: Connection -> IO [Account]
+allAccounts c = query_ c "SELECT account, mnemonic FROM stats.accounts;"
