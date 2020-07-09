553c12618359d9e4d236f0213c7c754c1d5c23f8
Indika Piyasena <indika.piyasena@gmail.com>
2020-07-10 09:14:13 +1000

    RPC2 bye bye

--- src/Radar/Scan.hs
+++ src/Radar/Scan.hs
@@ -17,9 +17,7 @@ type URL = String
 pairs =
   [ ("https://explorer.unification.io", "Unification Mainchain Explorer")
   , ("https://rest.unification.io/staking/validators", "operator_address")
-  , ("https://rest2.unification.io/staking/validators", "operator_address")
   , ("https://rpc1.unification.io:26657/status", "rpcrest-1")
-  , ("https://rpc2.unification.io:26657/status", "rpcrest-2")
   , ( "https://api-tokenswap.unification.io/pingtokenswap"
     , "bnb1hgk73jsfcg9achdmdrtn3h4pprjemfdhpdh3pn")
   , ( "https://api-tokenswap.unification.io/pingtokenswap"
